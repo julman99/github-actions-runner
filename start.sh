1329ff7af93d6abc9e7906348f5f83e481818fa9
Julio Viera <julio@fishbowlapp.com>
2024-05-03 18:25:54 -0400

    Enforces runner name specification and allows to override the group

--- start.sh
+++ start.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
+set -e
+
+if [ -z "${NAME}" ]; then
+    echo "Error, NAME must be specified via envvar"
+    exit 1
+fi
 
 if [ -z "${TOKEN}" ]; then
     echo "Error, TOKEN must be specified via envvar"
     exit 1
 fi
 
-./config.sh --unattended --url https://github.com/kluster-ai --token $TOKEN
+
+if [ -z "${RUNNER_GROUP}" ]; then
+    RUNNER_GROUP="default"
+fi
+
+./config.sh --unattended --url https://github.com/kluster-ai --token $TOKEN --replace --name "$NAME" --runnergroup "$RUNNER_GROUP"
 ./run.sh
